e271d233c28775d054da0283fb3cd3ba42568061
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2025-01-18 13:31:48 -0300

    <ISSUE-#1> | <Feature> | <Starting the initial project build>

--- src/main/java/com/interchallange/studyplan/StudyPlanApplication.java
+++ src/main/java/com/interchallange/studyplan/StudyPlanApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudyplanApplication {
+public class StudyPlanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudyplanApplication.class, args);
+		SpringApplication.run(StudyPlanApplication.class, args);
 	}
 
 }
